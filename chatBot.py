577db5c2c2a71ead33f2876fc2b853940fe2be6f
Admin <admin@Admins-MacBook-Pro.local>
2024-04-04 13:37:18 +0100

    working on new issues with chatbot

--- chatBot.py
+++ chatBot.py
@@ -1,44 +1,25 @@
+
+
 #initialise message
 print('''
 Wecome to UniBudyy! Your all-in-one app that makes your freshman journey a bit
 easier to navigate!
-        Please enter your credentials to get started!: 
+        Please enter your credentials to get started! 
     ''')
 
 #Getting and storing user input
-name = input("What is your name?: ")
-country = input("Where are you from?: ")
-age = input("How old are you?: ")
-colour = input("What is your favourite colour?: ")
+user_name = input("What is your name?: ")
+user_age = int((input("How old are you?: ")))
+user_colour = input("What is your favourite colour?: ").lower()
 
 #Use stored input to create personalised respones
 print(f"""
-Welcome {name}! I see that your favourite colour is {colour}.
-I have a few suggestions based on your choice! 
- 
-""")
-print("You are from {}, that's a great place.".format(country))
-
-#Control structures and while loops
-
-age = int(input("How old are you?: "))   
-if age < 18:
-    print("Wow!You are starting university at a young age!You must be really talented")
-elif age > 25 and age < 35:
-    print("Hm, you're much older then I expected")   
-elif age > 35:
-    print("That's fantastic! It's never too late to learn and grow")
-else:
-    print("{} is a fun age to start university at!".format(age))
-    
-    
-print(f"""
-Welcome{name}! I see that your favourite colour is {colour}.
+Welcome {user_name}! I see that your favourite colour is {user_colour}.
 I have a few suggestions based on your choice! 
  
 """)
 
-if colour == 'Red':
+if user_colour == 'red':
     print("If you like the colour RED, I have the following for you! Check out:  ")
     print("""
 1. Our red colour themed football club.
@@ -47,8 +28,8 @@ if colour == 'Red':
 4. Our twillight themed society.      
 """)
     
-elif colour == 'Blue':
-    print("If you like the colour RED, I have the following for you! Check out:  ")
+elif user_colour == 'blue':
+    print("If you like the colour BLUE, I have the following for you! Check out:  ")
 
     print("""
 1. Our fighting game club, where you can throw digital hands.
@@ -58,7 +39,7 @@ elif colour == 'Blue':
 5. Our bird watching society.      
 """)
     
-elif colour == 'Yellow':
+elif user_colour == 'yellow':
     
     print("If you like the colour YELLOW, I have the following for you! Check out:  ")
 
@@ -72,9 +53,27 @@ elif colour == 'Yellow':
        
 else:
     print("I am not sure if that's a colour")
-    
-    
-#FAQ
 
-question = input("Is there anything you would like to ask me? (Type in q to quit)")
+
+#Control structures and while loops
+
+
+if user_age < 18:
+    print("Wow!You are starting university at a young age!You must be really talented")
+elif user_age > 25 and user_age < 35:
+    print("Hm, you're much older then I expected")   
+elif user_age > 35:
+    print("That's fantastic! It's never too late to learn and grow")
+else:
+    print("{} is a fun age to start university at!".format(user_age))
     
+    
+
+    
+    
+#FAQ ; Frequently used question
+
+question = input("Is there anything you would like to ask me? (Type in q to quit: )")
+
+if question == 'How is your day':
+    print("I exist so my day is going well!")
